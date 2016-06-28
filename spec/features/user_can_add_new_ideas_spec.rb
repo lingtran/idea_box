00594c8569f22b5ae9da39c99af3eb14230180b2
lingtran <lxiutran@gmail.com>
2016-06-27 18:08:15 -0600

    pass request test for post for new idea

---
+++ spec/features/user_can_add_new_ideas_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "User can add new ideas", type: :feature do
+  xscenario "user can add a new idea upon submission" do
+    visit root_path
+
+    
+#     On the application's main page, a user should:
+#
+# See two text boxes for entering the "Title" and "Body" for a new idea, and a "Save" button for committing that idea. (3 points, mandatory for specification adherence)
+# When a user clicks "Save":
+#
+# A new idea with the provided title and body should appear in the idea list. (5 points, mandatory for specification adherence)
+# The text fields should be cleared and ready to accept a new idea. (2 points)
+# The page should not reload. (3 points, mandatory for specification adherence)
+# The idea should be committed to the database. It should still be present upon reloading the page. (2 points, mandatory for specification adherence)
+  end
+
+end
--- spec/requests/api_v1_ideas_controllers_spec.rb
+++ spec/requests/api_v1_ideas_controllers_spec.rb
@@ -24,4 +24,26 @@ RSpec.describe "Api::V1::IdeasController", type: :request do
       expect(response_body.last[:quality]).to eq(@genius_idea.quality)
     end
   end
+
+  describe "POST idea" do
+    before(:each) do
+      @swill_idea = create(:idea)
+      @genius_idea = create(:idea, :genius_idea)
+      @plausible_idea = create(:idea, :plausible_idea)
+      @new_idea = { title: "New Idea", body: "Body of new idea" }
+
+      post "/api/v1/ideas?title=#{@new_idea[:title]}&body=#{@new_idea[:body]}"
+    end
+
+    it "returns a response with for creating idea" do
+      expect(response).to have_http_status(201)
+      expect(response).to be_success
+    end
+
+    it "creates the idea" do
+      expect(@swill_idea[:title]).not_to eq(@new_idea[:title])
+      expect(Idea.last.title).to eq(@new_idea[:title])
+      expect(response_body[:title]).to eq(@new_idea[:title])
+    end
+  end
 end
