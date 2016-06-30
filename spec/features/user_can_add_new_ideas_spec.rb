253fd3a25e717ebca9372ab4fd8bc6fe117f8979
lingtran <lxiutran@gmail.com>
2016-06-30 00:51:37 -0600

    Pass feature test for creating new idea
    Pass test including JavaScript functionality

--- spec/features/user_can_add_new_ideas_spec.rb
+++ spec/features/user_can_add_new_ideas_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "User can add new ideas", type: :feature do
-  xscenario "user can add a new idea upon submission" do
-    existing_idea = create(:idea)
+  scenario "user can add a new idea upon submission", :js => true do
     new_idea = { title: "New idea title", body: "New idea body" }
 
     visit root_path
@@ -13,22 +12,12 @@ RSpec.describe "User can add new ideas", type: :feature do
       click_button("Save")
     end
 
-    within('.idea-index') do
-      expect(page).to have_content(new_idea[:title])
-      expect(page).to have_content(new_idea[:body])
-      expect(page).to have_content(existing_idea.title)
-      expect(page).to have_content(existing_idea.body)
-    end
+    wait_for_ajax
 
-#     On the application's main page, a user should:
-#
-# See two text boxes for entering the "Title" and "Body" for a new idea, and a "Save" button for committing that idea. (3 points, mandatory for specification adherence)
-# When a user clicks "Save":
-#
-# A new idea with the provided title and body should appear in the idea list. (5 points, mandatory for specification adherence)
-# The text fields should be cleared and ready to accept a new idea. (2 points)
-# The page should not reload. (3 points, mandatory for specification adherence)
-# The idea should be committed to the database. It should still be present upon reloading the page. (2 points, mandatory for specification adherence)
+    within('.ideas-table') do
+      expect(page.all('.ideas-list').first).to have_content(new_idea[:title])
+      expect(page.all('.ideas-list').first).to have_content(new_idea[:body])
+    end
   end
 
 end
