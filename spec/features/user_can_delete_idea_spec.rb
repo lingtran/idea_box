1bede3ac4b75eabcc76bd9d0284928ab0a0f327d
lingtran <lxiutran@gmail.com>
2016-06-30 02:19:55 -0600

    WIP figure out why length is not changing

---
+++ spec/features/user_can_delete_idea_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "User can delete idea", type: :feature do
+  xscenario "they can delete an idea", :js => true do
+    new_idea = { title: "New idea title", body: "New idea body" }
+
+    visit root_path
+
+    3.times do
+      within('.new-idea') do
+        fill_in "Title", with: new_idea[:title]
+        fill_in "Body", with: new_idea[:body]
+        click_button("Save")
+      end
+    end
+
+    original_count = page.all('.ideas-list').length
+
+    wait_for_ajax
+
+    within('.ideas-index') do
+      page.all('.delete-idea')[0].click
+    end
+
+    wait_for_ajax
+
+    new_count = page.all('.ideas-list').length
+
+    expect(new_count).not_to eq(original_count)
+  end
+end
